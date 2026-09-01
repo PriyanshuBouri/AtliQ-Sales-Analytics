aa558846354879ea7e6aa11943c340b24081fc10
PRIYANSHU KUMAR BOURI <rishu904b@gmail.com>
2026-09-02 00:52:47 +0530

    Add complete AtliQ Sales Analytics project

---
+++ stored_procedure_sql/get_top_n_customers_by_sales.sql
@@ -0,0 +1,19 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_top_n_customers_by_sales`(
+	in_market varchar(45),
+    in_fiscal_year int,
+    in_top_n int
+)
+BEGIN
+	#-------top 5 market by sales-------- 
+	SELECT 
+		c.customer,
+		round(sum(net_sales)/1000000,2) as net_sales_mln
+	FROM gdb0041.net_sales n
+	join dim_customer c
+	on
+		n.customer_code = c.customer_code
+	where fiscal_year = in_fiscal_year and c.market = in_market
+	group by c.customer
+	order by net_sales_mln desc
+	limit in_top_n;
+END
